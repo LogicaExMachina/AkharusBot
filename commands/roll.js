9c84471e28deba445d88358ed5f0d2dc615f563a
Robert L Quigley <lexm@pobox.com>
2018-09-06 16:51:18 -0400

    Teach roll command to handle multi roll + modifiers
    Split roll functionality out into a distinct function for looping.  This
    facilitates rolling multiple times with a ; seperator.  Add basic modifier
    logic.

--- commands/roll.js
+++ commands/roll.js
@@ -3,27 +3,44 @@ module.exports = {
     description: 'RollDice',
     execute(message, args) {
 			var content = args.join();
-			if(content.includes("d")) {
-				//split on d
-				var re = /([0-9]*)[dD]([0-9]+)/;
-				res = re.exec(content);
-				var result = 0;
-				console.log(res);
-				if(null != res) {
-					for(var i = 0; i< res[1]; i++) {
-						var die = Math.floor(Math.random()*res[2])+1;
-						result += die;
-						console.log("Die: " + die);
-					}
-				}
-			}
-			else {
-				size = parseInt(args);
-				if(!isNaN(size)) {
-					result = Math.floor(Math.random()*size)+1;
-				}
-			}
+			var seq = content.split(';');
+			var result = "";
+			const sep = "";
+			seq.forEach((e) => {
+				t = roll(e);
+				console.log(t);
+				result += t + "\n";
+
+			});
 			message.channel.send(result);
 		}
 };
 
+function roll(value) {
+	var result = 0;
+	if(value.includes("d")) {
+		var re = /([0-9]*)[dD]([0-9]+)([\-+][0-9]+)*/;
+		res = re.exec(value);
+		console.log(res);
+		if(null != res) {
+			var count = res[1];
+			var size = res[2];
+			var mod = parseInt(res[3]);
+			if(isNaN(mod)) {
+				mod = 0;
+			}
+			for(var i = 0; i< count; i++) {
+				var die = Math.floor(Math.random()*size)+1;
+				result += die;
+			}
+			result += mod;
+		}
+	}
+	else {
+		size = parseInt(value);i
+		if(!isNaN(size)) {
+			result = Math.floor(Math.random()*size)+1;
+		}
+	}
+	return result;
+}
