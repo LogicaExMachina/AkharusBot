4803fad7547054badbede346e97e0925c5af6440
Robert L Quigley <lexm@pobox.com>
2018-09-06 13:15:27 -0400

    Teach roll to handle multiple dice
    Implement basic parsing to handle XdY syntax.
    
    TODO: Include modifiers

--- commands/roll.js
+++ commands/roll.js
@@ -2,11 +2,28 @@ module.exports = {
     name: 'roll',
     description: 'RollDice',
     execute(message, args) {
-			size = parseInt(args);
-			console.log(`Size: ${size}...`);
-			if(!isNaN(size)) {
-				result = Math.floor(Math.random()*size)+1;
-				message.channel.send(result);
+			var content = args.join();
+			if(content.includes("d")) {
+				//split on d
+				var re = /([0-9]*)[dD]([0-9]+)/;
+				res = re.exec(content);
+				var result = 0;
+				console.log(res);
+				if(null != res) {
+					for(var i = 0; i< res[1]; i++) {
+						var die = Math.floor(Math.random()*res[2])+1;
+						result += die;
+						console.log("Die: " + die);
+					}
+				}
 			}
+			else {
+				size = parseInt(args);
+				if(!isNaN(size)) {
+					result = Math.floor(Math.random()*size)+1;
+				}
+			}
+			message.channel.send(result);
 		}
 };
+
